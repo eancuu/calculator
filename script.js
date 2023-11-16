65e768e68a81eecf83cc757ef5eb8a833cf6c8ca
Ian <149064794+eancuu@users.noreply.github.com>
2023-11-17 06:49:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const array = [];
+
+let a;
+let b;
+
+document.getElementById('one').onclick = () =>{
+    array.push(1)
+    document.getElementById('result').innerHTML = array_string;
+}
+
+
+document.getElementById('submit').onclick = () =>{
+
+}
